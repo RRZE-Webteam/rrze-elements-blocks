3a4b5ede27558943c04cde780c42d98eb40a4805
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2023-09-20 16:59:58 +0200

    Styling adjustments

--- build/collapsibles/index.asset.php
+++ build/collapsibles/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5717cdd663c4f7eb1c55');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '653e0d1511b306347dc5');
