44ee9fb399f43b9b75f81f2d1befab5c1275d1ed
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2023-09-25 13:49:15 +0200

    Accordions | Restrict inner block usage

--- build/accordion/index.asset.php
+++ build/accordion/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '75e1cca214901c785db2');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '89bdb6a6d18c9e618c77');
