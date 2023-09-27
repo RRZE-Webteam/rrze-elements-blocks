3311d266f07356ddd54d724320c265aa2f11833c
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2023-09-27 15:03:27 +0200

    npm run build

--- build/accordion/index.asset.php
+++ build/accordion/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'd18ca2ab6bf7f266e950');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ce3fe0df7c17632050d5');
--- build/collapsibles/index.asset.php
+++ build/collapsibles/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '8680c62451ef3a9db156');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '03df9f6dbebcfcecd395');
