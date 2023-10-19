130ef8e9798a32870482f0e0ccdeeee0283a6751
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2023-10-19 13:37:14 +0200

    tabs | optimizer icon usage inside the xray component

--- build/tabs/index.asset.php
+++ build/tabs/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9e4b84b64a293d93d6ff');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e0429dbb8d7b8a0bce23');
