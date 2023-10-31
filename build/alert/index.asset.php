0399eb2b2b753ce57ae37d05ffc1c223ad6b23e1
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2023-10-31 08:22:59 +0100

    cta | Add basic styling

--- build/alert/index.asset.php
+++ build/alert/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => 'e6939e5da1ec46793a65');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '0a6bee261e824b20bb97');
--- build/cta/index.asset.php
+++ build/cta/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'd8a6a2f95d424b7fd193');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'f15dc41e2dca6c78770c');
