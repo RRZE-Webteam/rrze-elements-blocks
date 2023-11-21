cb52ebe941f72344b0837d393cf6dc505005baa2
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2023-11-21 16:18:59 +0100

    cwl | Iconset
    Add custom Icon

--- build/cta/index.asset.php
+++ build/cta/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-notices', 'wp-primitives'), 'version' => '8ef4fcd31af80b3d3dbe');
+<?php return array('dependencies' => array('react', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-notices', 'wp-primitives'), 'version' => '4284c62db9811da0bf5d');
--- src/contentwidthlimiter/index.js
+++ src/contentwidthlimiter/index.js
@@ -28,15 +28,14 @@ import { __, sprintf } from '@wordpress/i18n';
  *
  * @see https://developer.wordpress.org/block-editor/developers/block-api/#registering-a-block
  */
-registerBlockType( metadata.name as any, {
+registerBlockType( metadata.name, {
 	/**
 	 * Used to construct a preview for the block to be shown in the block inserter.
 	 */
 	icon: {
-		src: "minus",
-		background: "#00458c"
+		src: <svg id="Ebene_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><rect x="77.51" y="114.93" width="153.67" height="280.62" rx="5.73" ry="5.73" fill="evenodd" strokeWidth="0"/><g opacity=".25"><rect x="175.76" y="114.93" width="271.12" height="280.62" rx="5.73" ry="5.73" fill="evenodd" strokeWidth="0"/></g><path d="m475.16,248.35v20.36c0,4.29-3.46,7.76-7.76,7.76h-173.77l40.78,40.78c3.03,3.03,3.03,7.94,0,10.96l-2.1,2.1c-3.02,3.03-7.93,3.03-10.96,0l-64.76-64.76c-2.98-2.99-3.02-7.81-.11-10.84.38-.88.92-1.71,1.64-2.42l64.76-64.76c3.03-3.02,7.93-3.02,10.96,0l2.1,2.1c3.03,3.03,3.03,7.93,0,10.96l-40.02,40.02h171.49c4.29,0,7.76,3.46,7.76,7.76Z" fill="evenodd" strokeWidth="0"/></svg>
 	},
-	__experimentalLabel: (attributes: any, { context }: any) => {
+	__experimentalLabel: (attributes, { context }) => {
 		const { title } = attributes;
 
 		// In the list view, use the block's title as the label.
@@ -54,4 +53,4 @@ registerBlockType( metadata.name as any, {
 	 * @see ./save.js
 	 */
 	save,
-} as any );
+} );
