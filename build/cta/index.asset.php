dfcbcfaaf313d167252ac115edb5e9bb21421798
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2023-11-23 17:03:52 +0100

    cta | Add example for block inserter preview

--- build/cta/index.asset.php
+++ build/cta/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-notices', 'wp-primitives'), 'version' => '4284c62db9811da0bf5d');
+<?php return array('dependencies' => array('react', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-notices', 'wp-primitives'), 'version' => 'b46bc8a810679c1c73f1');
