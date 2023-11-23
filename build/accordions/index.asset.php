cf9ad185c9c34580583dd8cc30de1efd924f9734
Barbara Bothe <barbara.bothe@fau.de>
2023-11-23 13:53:46 +0100

    CSS accordion/collapse

--- build/accordions/index.asset.php
+++ build/accordions/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element'), 'version' => '477fc23cc8ec985da0f2');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element'), 'version' => 'ad791ddfe3ed668e0129');
