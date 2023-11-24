94b1f508d232f80a501e93a23ac4ce0219d10ad9
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2023-11-24 11:01:43 +0100

    cwl | Fix the minimum and maximum values

--- build/contentwidthlimiter/index.asset.php
+++ build/contentwidthlimiter/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '4f3bd7b8429cf1d51160');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c7bfbf126f3d4ba6286d');
