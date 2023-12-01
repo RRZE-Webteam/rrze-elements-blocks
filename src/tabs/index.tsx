270da216586b0a3a5208bfd4d6fb53b533c12b7f
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2023-12-01 15:37:44 +0100

    Examples for BlockInserter
    Add Example values for Block Inserter

--- src/tabs/index.tsx
+++ src/tabs/index.tsx
@@ -31,11 +31,6 @@ registerBlockType( metadata.name as any, {
 	/**
 	 * Used to construct a preview for the block to be shown in the block inserter.
 	 */
-	example: {
-		attributes: {
-			message: 'Accordion',
-		},
-	},
 	icon: {
 		src: <svg id="Ebene_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g opacity=".5"><rect x="75.86" y="204.25" width="360.29" height="133.91" fill="evenodd" strokeWidth="0"/></g><path d="m81.59,173.84h112.67c3.16,0,5.73,2.57,5.73,5.73v25.16h-124.13v-25.16c0-3.16,2.57-5.73,5.73-5.73Z" fill="evenodd" strokeWidth="0"/><path d="m150.48,173.84h112.67c3.16,0,5.73,2.57,5.73,5.73v25.16h-124.13v-25.16c0-3.16,2.57-5.73,5.73-5.73Z" fill="evenodd" opacity=".2" strokeWidth="0"/></svg>
 	},
