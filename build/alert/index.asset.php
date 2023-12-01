13347538398d6ca5d0803ad4eb85572f00969b99
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2023-12-01 13:27:25 +0100

    ColorSelector | Use ThemeJson Defaults
    Use the ThemeJson Colors instead, if they exist

--- build/alert/index.asset.php
+++ build/alert/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '3c3d3435caf888aa43f4');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '873f9c6c3b729779ba4c');
