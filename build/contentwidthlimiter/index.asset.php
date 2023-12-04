1d410ff55309943d16d8ae8a26fd778c66f2bdb0
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2023-12-04 16:08:41 +0100

    AlignmentBar | Add Support for center control
    Add center as alignment option

--- build/contentwidthlimiter/index.asset.php
+++ build/contentwidthlimiter/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '6f57c2a890f0cbfcbce3');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'd93c4599b5a95feb221a');
--- build/insertion/index.asset.php
+++ build/insertion/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '7caf80e3f5e3b40e51a2');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => 'b917d5af73171e147fb5');
