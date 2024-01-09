7670a09fd1ed985a020a21bd24e532800fdb87c3
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2024-01-09 13:49:10 +0100

    style(accordion): Improves the editor view

--- build/accordion/index.asset.php
+++ build/accordion/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '904811d28e50950829bc');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '02191861cd3125d12268');
--- build/collapse/index.asset.php
+++ build/collapse/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'b4a3d77b6a79349075c3');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ee3575a7acec63d0c413');
