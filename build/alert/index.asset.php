347cacfae196f8eda01bf154431d35cd771ea6a6
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2024-01-10 14:07:48 +0100

    fix(alert): Remove unnecessary inline styles

--- build/alert/index.asset.php
+++ build/alert/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '9fe304ca583c7bfd020e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '2d92fa02d8aa5077080a');
