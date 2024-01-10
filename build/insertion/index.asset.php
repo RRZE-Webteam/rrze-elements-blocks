5f2d2333b19d9f4a6f436786a67bcc6ff671156c
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2024-01-10 10:40:27 +0100

    !feat(text-slider) Remove the Textslider for v1
    - Textslider will be available in later versions

--- build/insertion/index.asset.php
+++ build/insertion/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => 'f3bb33ce9f17ebcd8ea0');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '18c9497599cd73852de5');
