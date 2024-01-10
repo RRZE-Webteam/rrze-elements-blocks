9b3ae08df30e59a8385188ce71252f9f9941a0dd
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2024-01-10 13:53:07 +0100

    fix(contentwidthlimiter) - Alignment in Editor view
    - Fixes the standard alignment in Editor view

--- build/contentwidthlimiter/index.asset.php
+++ build/contentwidthlimiter/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '721ba2d60ce65dcaebcc');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '3aea105367aced3f20e5');
