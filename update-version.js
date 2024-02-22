c4960b2e6e72902e34976a69b40dee61cfbc9f77
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2024-02-22 12:55:41 +0100

    feat(npm update-version): Update all version numbers via NPM script
    This script reads the package.json version number and applies it to:
    - Main php plugin file
    - Main php plugin file PHP constant RRZE_ELEMENTSB
    - All block src files

---
+++ update-version.js
@@ -0,0 +1,60 @@
+/**
+ * This script updates the version number for the main php file and all block.json files
+ */
+
+const fs = require("fs");
+const packageJson = require("./package.json");
+
+const updateVersion = (filePath, version) => {
+  let fileContent = fs.readFileSync(filePath, "utf8");
+
+  // Update the version number in the main plugin file
+  if (filePath.endsWith("rrze-elements-blocks.php")) {
+    // Refresh the version mentioned inside the WP Plugin comment
+    fileContent = fileContent.replace(
+      /Version:\s*\d+\.\d+\.\d+/,
+      `Version:         ${version}`
+    );
+
+    // Refresh the version number for RRZE_ELEMENTSB_VERSION constant
+    fileContent = fileContent.replace(
+      /const RRZE_ELEMENTSB_VERSION = '\d+\.\d+\.\d+';/,
+      `const RRZE_ELEMENTSB_VERSION = '${version}';`
+    );
+  }
+  // Update the version number in all blocks
+  else if (filePath.endsWith("block.json")) {
+    const jsonContent = JSON.parse(fileContent);
+    jsonContent.version = version;
+    fileContent = JSON.stringify(jsonContent, null, 2);
+  }
+
+  fs.writeFileSync(filePath, fileContent, "utf8");
+};
+
+// Use the version number from package.json
+const version = packageJson.version;
+
+// Update the following files
+const filesToUpdate = [
+  "./rrze-elements-blocks.php",
+  "./src/accordion/block.json",
+  "./src/accordions/block.json",
+  "./src/alert/block.json",
+  "./src/collapse/block.json",
+  "./src/block-blueprint/block.json",
+  "./src/collapse/block.json",
+  "./src/collapsibles/block.json",
+  "./src/columns/block.json",
+  "./src/contentwidthlimiter/block.json",
+  "./src/cta/block.json",
+  "./src/insertion/block.json",
+  "./src/news/block.json",
+  "./src/notice/block.json",
+  "./src/tab/block.json",
+  "./src/tabs/block.json",
+  "./src/textslider/block.json",
+  "./src/textslideritem/block.json",
+]; // usw.
+
+filesToUpdate.forEach((filePath) => updateVersion(filePath, version));
