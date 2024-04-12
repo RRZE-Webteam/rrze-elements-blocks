f2b06ba66398039db7137b00a068bf2f6c92880e
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2024-04-12 13:51:12 +0200

    feat(counter) – Add style outline and colorblock
    Adds two styling options for the counter row

--- build/counter/index.asset.php
+++ build/counter/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => 'a3b56e47a3516baf8dfd');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => '11cb0b6f5018a8dd1284');
