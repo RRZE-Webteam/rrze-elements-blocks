4fb06fb4706d43110e6966d1a04c145eb913b739
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2024-04-12 15:25:08 +0200

    feat(rrze-counter.js) – Fix ScrollTrigger and Stagger
    Fixes an issue where ScrollTriggers did not work as expected with several containers on a page.

--- assets/js/counter/rrze-counter.js
+++ assets/js/counter/rrze-counter.js
@@ -1,56 +1,58 @@
-function numberWithDots(x){
+function numberWithDots(x) {
   if (x == null) { 
-    console.log('Received null or undefined');
-    return '0';
+      console.log('Received null or undefined');
+      return '0';
   }
 
   let cleanInput = x.toString().replace(/\./g, ''); // Remove any dots in the string
   const number = parseInt(cleanInput, 10);
   if (isNaN(number)) {
-    console.log('Conversion to number failed');
-    return 'Invalid number';
+      console.log('Conversion to number failed');
+      return 'Invalid number';
   }
 
   const numberAsString = number.toString();
   return numberAsString.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
 
-
 try {
   gsap.registerPlugin(ScrollTrigger);
 
-  const prefersReducedMotion = window.matchMedia(
-    "(prefers-reduced-motion: reduce)"
-  ).matches;
-  const items = gsap.utils.toArray(".fau-counter-data"); // Convert NodeList to an array
+  const prefersReducedMotion = window.matchMedia("(prefers-reduced-motion: reduce)").matches;
+  //const counterContainers = document.querySelectorAll('.wp-block-rrze-elements-rrze-counter');
+  const counterContainers = document.querySelectorAll('.rrze--counter-element');
 
   if (!prefersReducedMotion) {
-    // Access the data-stagger attribute from the section element
-    const counterRow = document.querySelector('.rrze--counter-element');
-    const staggerValue = parseFloat(counterRow.getAttribute('data-stagger'));
-    console.log(staggerValue);
+      counterContainers.forEach(container => {
+          const items = container.querySelectorAll(".fau-counter-data");
+          const counterRow = container.closest('.rrze--counter-element');
+          const staggerValue = parseFloat(counterRow.getAttribute('data-stagger'));
 
-    gsap.from(items, {
-      textContent: 0,
-      duration: 2,
-      ease: "power3.inOut",
-      stagger: staggerValue,
-      snap: { textContent: 1 },
-      onUpdate: function () {
-        this.targets().forEach((target) => {
-          target.innerHTML = numberWithDots(Math.ceil(parseInt(target.textContent.replace(/\./g, ''), 10)));
-        });
-      },
-      scrollTrigger: {
-        trigger: items[0],
-        start: "top 80%",
-        toggleActions: "play none none none",
-      },
-    });
+          gsap.from(items, {
+              textContent: 0,
+              duration: 2,
+              ease: "power3.inOut",
+              stagger: staggerValue || 0,
+              snap: { textContent: 1 },
+              onUpdate: function () {
+                  this.targets().forEach((target) => {
+                      target.innerHTML = numberWithDots(Math.ceil(parseInt(target.textContent.replace(/\./g, ''), 10)));
+                  });
+              },
+              scrollTrigger: {
+                  trigger: container,
+                  start: "top 80%",
+                  toggleActions: "play none none none",
+              },
+          });
+      });
   } else {
-    items.forEach((item) => {
-      item.textContent = numberWithDots(parseInt(item.textContent, 10));
-    });
+      counterContainers.forEach(container => {
+          const items = container.querySelectorAll(".fau-counter-data");
+          items.forEach((item) => {
+              item.textContent = numberWithDots(parseInt(item.textContent, 10));
+          });
+      });
   }
 } catch (error) {
   console.error("Animation initialization failed:", error);
