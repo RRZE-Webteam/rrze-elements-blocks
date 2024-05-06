db0302bc2df8afd5335de6f90a08895d886785b5
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2024-05-06 10:52:48 +0200

    fix(notices) - Change the visual appearance 2

--- build/notice/index.asset.php
+++ build/notice/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '307f1025b56f533df9e6');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '1f8d6a424f8b72675cd8');
