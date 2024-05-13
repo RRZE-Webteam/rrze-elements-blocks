6a90e72936f4474180b602992790fd87a6d0d3ec
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2024-05-13 15:34:39 +0200

    fix(textcolumns) - Fix the color-classes

--- build/columns/index.asset.php
+++ build/columns/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8ef5be097bbddfc2b927');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '1c2ab28e5acdd0fc11ce');
