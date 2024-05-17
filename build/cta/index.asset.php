02802022816641178873a1d08b7df4fdfb63bde5
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2024-05-17 12:29:16 +0200

    fix(cta) - Fix CSS issues
    - Resolves an CSS issue where the image did not fill the cta box completely on the vertical axis

--- build/cta/index.asset.php
+++ build/cta/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-notices', 'wp-primitives'), 'version' => '594a6644a73a02ba41d9');
+<?php return array('dependencies' => array('react', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-notices', 'wp-primitives'), 'version' => 'c3b74d5b6fafc5bed6d0');
