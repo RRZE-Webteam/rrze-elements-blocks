23a3594aa85fc82f271f2c0066fb9524d5060a28
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2024-06-05 16:15:53 +0200

    fix(counter-row) - Fix the frontend grid view
    Removes the feature to adapt the size automatically for better control of the layout

--- build/counter-row/index.asset.php
+++ build/counter-row/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '77ecc0164063e98bea59');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '9aea3ffd12c5ac30f049');
