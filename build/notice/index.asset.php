28de0c32fb70e20ab4b08f7d81525bdc3e411493
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2024-06-05 16:29:38 +0200

    fix(notices) - Minor changes to Variant selection
    Changes the Hint icon inside the variant picker

--- build/notice/index.asset.php
+++ build/notice/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'eb3cf4281d8b137f6fc9');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'a7ed37ea555cfe210023');
