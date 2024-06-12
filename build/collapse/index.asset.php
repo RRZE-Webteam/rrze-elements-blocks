1125a1b96491441a5ab67b933a2da47f64fd54fc
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2024-06-12 12:51:28 +0200

    fix(collapse) - Deprecation API
    Resolves #116 - Deprecation API for v1.0.3

--- build/collapse/index.asset.php
+++ build/collapse/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '6487be3db61df9694d94');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '78da8717ce54d65d9357');
