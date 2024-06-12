93648a36b75ea3104cf26bcf8ced76cdddfe51e3
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2024-06-12 12:44:52 +0200

    fix(collapsibles) - Deprecation API
    Resolves #116 – Deprecation API is running

--- build/collapsibles/index.asset.php
+++ build/collapsibles/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '4dd41ceee6fcfe4b04da');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'b26578b4cf67afc3413d');
