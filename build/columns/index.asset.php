e286ab18cb6f4c6415abaed68da9dc5c5364847b
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2024-06-25 16:01:38 +0200

    feat(CustomColorSwitcher) - Solve deprecation warning
    Replace useSetting with useSettings. Add an index to the received array. Resolves #126

--- build/columns/index.asset.php
+++ build/columns/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e98f6ec272bf1f853b20');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '001ea4c16c1655cf6c7e');
