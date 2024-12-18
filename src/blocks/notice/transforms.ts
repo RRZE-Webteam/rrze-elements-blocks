e9d5f4e71d04b3236ed845a3f25f3e1aee986f43
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2024-12-18 10:34:50 +0100

    feat(notices) - Add transforms support to and from blocks
    Adds transforms support from and to blocks. wip #175

---
+++ src/blocks/notice/transforms.ts
@@ -0,0 +1,48 @@
+import { createBlock, BlockInstance } from "@wordpress/blocks";
+
+const transforms = {
+  from: [
+    {
+      type: 'block' as const,
+      isMultiBlock: true,
+      blocks: ["core/paragraph"],
+      isMatch: (_attributes: any, blocks: any) => {
+        return !blocks.some((block: any) => block.name === 'rrze-elements/notice');
+      },
+
+      __experimentalConvert: (blocks: BlockInstance[]) => {
+        const columns: BlockInstance[] = [];
+
+        blocks.forEach((block) => {
+          columns.push(
+            createBlock(
+              block.name,
+              block.attributes,
+              block.innerBlocks
+            )
+          );
+        });
+
+        return createBlock<any>(
+          'rrze-elements/notice',
+          {},
+          columns
+        );
+      },
+    },
+  ],
+  to: [
+    {
+      type: 'block' as const,
+      blocks: ['core/paragraph'],
+      transform: (
+        attributes: any,
+        innerBlocks: BlockInstance[]
+      ): BlockInstance[] => {
+        return innerBlocks;
+      },
+    },
+  ],
+};
+
+export default transforms;
