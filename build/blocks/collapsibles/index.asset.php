b3a13768227109c7c9611d473124e262c59c771b
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-02-12 14:35:03 +0100

    feat(accordion) - Update CSS for the frontend
    #196

--- build/blocks/collapsibles/index.asset.php
+++ build/blocks/collapsibles/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '92314b2002b8cce8f235');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c4c19f36becd550770d1');
