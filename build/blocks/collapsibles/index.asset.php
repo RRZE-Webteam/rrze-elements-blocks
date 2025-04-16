a6b00f38910391fc8cd719eab4a7f1611cf0a682
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-04-16 15:45:01 +0200

    run(build) - Build files
    npm run build

--- build/blocks/collapsibles/index.asset.php
+++ build/blocks/collapsibles/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '4d9b708326a7e9c6516d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '76069daca8e6db4cae09');
