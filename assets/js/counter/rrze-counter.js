e928f36adeec70a20b23819e4437dd2e16435d0d
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-05-12 18:06:08 +0200

    fix(#233) - Resolves Timeline & Counter animation issues
    This fix aims to fix the gsap animations for the counter-row by resetting the scroll position on load.

--- assets/js/counter/rrze-counter.js
+++ assets/js/counter/rrze-counter.js
@@ -16,49 +16,64 @@ function numberWithDots(x) {
 }
 
 try {
-  gsap.registerPlugin(ScrollTrigger);
+  console.log("try");
+  ScrollTrigger.clearScrollMemory();
+  window.scrollTo(0, 0);
+  window.addEventListener("load", () => {
+    gsap.registerPlugin(ScrollTrigger);
+    console.log("Dom Content Loaded");
+    console.log("gsap register Plugin");
 
-  const prefersReducedMotion = window.matchMedia(
-    "(prefers-reduced-motion: reduce)"
-  ).matches;
-  //const counterContainers = document.querySelectorAll('.wp-block-rrze-elements-rrze-counter');
-  const counterContainers = document.querySelectorAll(".rrze--counter-element");
+    const prefersReducedMotion = window.matchMedia(
+      "(prefers-reduced-motion: reduce)"
+    ).matches;
+    //const counterContainers = document.querySelectorAll('.wp-block-rrze-elements-rrze-counter');
+    const counterContainers = document.querySelectorAll(".rrze--counter-element");
+    console.log("counterContainers");
+    console.log(counterContainers);
+    console.log(counterContainers.length);
 
-  if (!prefersReducedMotion) {
-    counterContainers.forEach((container) => {
-      const items = container.querySelectorAll(".fau-counter-data");
-      const counterRow = container.closest(".rrze--counter-element");
-      const staggerValue = parseFloat(counterRow.getAttribute("data-stagger"));
-      const startValue = parseFloat(counterRow.getAttribute("data-startValue"));
+    if (!prefersReducedMotion) {
+      counterContainers.forEach((container) => {
+        const items = container.querySelectorAll(".fau-counter-data");
+        console.log(items);
+        const counterRow = container.closest(".rrze--counter-element");
+        console.log(counterRow);
+        const staggerValue = parseFloat(counterRow.getAttribute("data-stagger"));
+        console.log(staggerValue);
+        const startValue = parseFloat(counterRow.getAttribute("data-startValue"));
+        const endValue = parseFloat(counterRow.getAttribute("data-endValue"));
 
-      gsap.from(items, {
-        textContent: startValue || 0,
-        duration: 2,
-        ease: "power3.inOut",
-        stagger: staggerValue || 0,
-        snap: { textContent: 1 },
-        onUpdate: function () {
-          this.targets().forEach((target) => {
-            target.innerHTML = numberWithDots(
-              Math.ceil(parseInt(target.textContent.replace(/\./g, ""), 10))
-            );
-          });
-        },
-        scrollTrigger: {
-          trigger: container,
-          start: "top 80%",
-          toggleActions: "play none none none",
-        },
+        gsap.from(items, {
+          textContent: startValue || 0,
+          duration: 2,
+          ease: "power3.inOut",
+          stagger: staggerValue || 0,
+          snap: {textContent: 1},
+          onUpdate: function () {
+            this.targets().forEach((target) => {
+              target.innerHTML = numberWithDots(
+                Math.ceil(parseInt(target.textContent.replace(/\./g, ""), 10))
+              );
+            });
+          },
+          scrollTrigger: {
+            trigger: container,
+            start: "top bottom",
+            toggleActions: "play none none none",
+          },
+        });
+        console.log("finished");
       });
-    });
-  } else {
-    counterContainers.forEach((container) => {
-      const items = container.querySelectorAll(".fau-counter-data");
-      items.forEach((item) => {
-        item.textContent = numberWithDots(parseInt(item.textContent, 10));
+    } else {
+      counterContainers.forEach((container) => {
+        const items = container.querySelectorAll(".fau-counter-data");
+        items.forEach((item) => {
+          item.textContent = numberWithDots(parseInt(item.textContent, 10));
+        });
       });
-    });
-  }
+    }
+  })
 } catch (error) {
   console.error("Animation initialization failed:", error);
 }
