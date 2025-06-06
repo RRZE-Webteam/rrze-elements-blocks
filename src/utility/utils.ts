1d601dbca337048c5f5da289115b22d242f6b406
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-06-06 14:08:53 +0200

    fix(utils) - Sanitizes titles for jump names more robustly.
    Enhances the title sanitization process to create more reliable jump names.
    
    This includes:
    - Removal of HTML tags.
    - Removal of common HTML entities.
    
    These changes improve the accuracy and consistency of jump name generation.
    
    Resolves #239, #238

--- src/utility/utils.ts
+++ src/utility/utils.ts
@@ -2,18 +2,37 @@
  * Utility function to create JumpName out of title attributes
  */
 export function sanitizeTitleToJumpName(title: string): string {
-    if (!title) return "";
-    let sanitized = title.toLowerCase();
+  if (!title) return "";
 
-    sanitized = sanitized
-        .replace(/ö/g, 'oe')
-        .replace(/ä/g, 'ae')
-        .replace(/ü/g, 'ue')
-        .replace(/ß/g, 'ss');
+  // removal of html tags
+  let sanitized = title.replace(/<[^>]*>/g, '');
 
-    sanitized = sanitized
-        .replace(/\s+/g, '-')
-        .replace(/[^a-z0-9-]/g, '');
+  // removal of common entities
+  const entityMap: { [key: string]: string } = {
+    '&amp;': '&',
+    '&#38;': '&',
+    '&lt;': '<',
+    '&gt;': '>',
+    '&quot;': '"',
+    '&#39;': "'"
+  };
 
-    return sanitized;
+  Object.keys(entityMap).forEach(entity => {
+    const regex = new RegExp(entity, "g");
+    sanitized = sanitized.replace(regex, entityMap[entity]);
+  });
+
+  sanitized = sanitized.toLowerCase();
+
+  sanitized = sanitized
+    .replace(/ö/g, 'oe')
+    .replace(/ä/g, 'ae')
+    .replace(/ü/g, 'ue')
+    .replace(/ß/g, 'ss');
+
+  sanitized = sanitized
+    .replace(/\s+/g, '-')
+    .replace(/[^a-z0-9-]/g, '');
+
+  return sanitized;
 }
