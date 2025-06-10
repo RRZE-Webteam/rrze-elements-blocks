2e0bd50371731b468c1f4b998ecb7dfbf56ba60c
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-06-10 16:45:25 +0200

    run(build)

--- build/blocks/collapse/index.asset.php
+++ build/blocks/collapse/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-a11y', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ff29e5475154ee0945f0');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-a11y', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '93609a26d2b0506cddb8');
