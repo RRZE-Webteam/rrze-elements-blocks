b4980f89451b58a2bdd209f28848f12801562d6b
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-06-10 08:16:27 +0200

    run(build)

--- build/blocks/alert/index.asset.php
+++ build/blocks/alert/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => '6797252672bcfb5cc659');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => '1881a9a9b826933d8fd5');
