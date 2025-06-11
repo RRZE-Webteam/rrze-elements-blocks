d14c802621cb2e8c63f84c68ba3a218c720f4a65
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-06-11 18:20:04 +0200

    run(build)

--- build/blocks/collapsibles/index.asset.php
+++ build/blocks/collapsibles/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '670ea229b116bf310d22');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '7d5c1f80452a12929403');
