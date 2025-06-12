ae2a9e161c9e188e0a32ae8e528f5f6a76774277
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-06-12 17:57:17 +0200

    run(build)

--- build/blocks/timeline-item/index.asset.php
+++ build/blocks/timeline-item/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '8641c46c500aa2657ae2');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '9de0f757eb002886fa7d');
