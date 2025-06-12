cc6a806a05dfd36c2abef9d87397ef5c4a7c39cf
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-06-12 13:00:18 +0200

    run(build)

--- build/blocks/counter-row/index.asset.php
+++ build/blocks/counter-row/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'd03d6d430a5a8d840883');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'd3f0ca2da10293984dcc');
