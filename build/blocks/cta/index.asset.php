2053ce05112cfc2cce9a1c5df6777621578f1afd
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-06-13 14:50:06 +0200

    run(build)

--- build/blocks/cta/index.asset.php
+++ build/blocks/cta/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-notices', 'wp-primitives'), 'version' => 'f13d0c666ed42786d5af');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-notices', 'wp-primitives'), 'version' => '7618df18e569020f5469');
