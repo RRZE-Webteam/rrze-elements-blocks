2b890bab055ab1325b361572f0a3b835e9a78c95
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-07-03 08:59:11 +0200

    feat(css) - Prepare CSS for FAU-Elemental

--- build/blocks/collapsibles/index.asset.php
+++ build/blocks/collapsibles/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '7291ca1179c4581435a9');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '8185ca8a41f7f414bf90');
