1952314f97ce64c49edb903f0c1006ee48838d9c
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-07-11 16:34:26 +0200

    fixes(alert): Updates font family for non-FAU themes
    Ensures the correct font family (Roboto) is applied to text elements within the alert block when the FAU Elemental theme is not active.
    
    This provides consistency and correct styling across different themes.

--- build/blocks/alert/index.asset.php
+++ build/blocks/alert/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => '62c3f73957897a10ef42');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => '5ce995adf6bd6785ff00');
