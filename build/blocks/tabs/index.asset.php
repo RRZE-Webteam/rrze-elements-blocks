e5d2f239bd29ba885308b6c85907b37516beb7ba
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-07-11 16:44:01 +0200

    css(tabs): Prepare CSS and assets for FAU-Elemental
    This commit generates the necessary CSS and asset files for the tabs block, including separate CSS files for RTL and LTR layouts, and updates the asset manifest.

--- build/blocks/tabs/index.asset.php
+++ build/blocks/tabs/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react-jsx-runtime', 'wp-a11y', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '85bc2945d730fa66bf14');
+<?php return array('dependencies' => array('lodash', 'react-jsx-runtime', 'wp-a11y', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '8c36dc71c091212c211c');
