b126b85aa1bfbf19b7aca0f1acf57dfca7398959
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-07-25 10:45:35 +0200

    fix(css) - Prepare CSS for FAU-Elemental

--- build/blocks/notice/index.asset.php
+++ build/blocks/notice/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-a11y', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '2212cf1b43e00ff266c5');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-a11y', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '27ee8502778fda144f58');
