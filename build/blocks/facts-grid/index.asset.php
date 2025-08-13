28d4da981923d3d626ec4d98b282ab2cafa76022
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-08-13 13:50:08 +0200

    feat(facts-grid) - Add ability to hide Title
    Adds the ability to hide the Title inside the Facts Grid.

--- build/blocks/facts-grid/index.asset.php
+++ build/blocks/facts-grid/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e8f5c1bfae17f97fbfce');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '621987c89c42a424bf4d');
--- includes/BlockFrontend/FactsGrid.php
+++ includes/BlockFrontend/FactsGrid.php
@@ -23,22 +23,28 @@ class FactsGrid extends AbstractBlockRender
       ? esc_attr($attributes['anchor'])
       : 'facts-heading';
     $headingLevel = isset($attributes['headingLevel']) ? absint($attributes['headingLevel']) : 3;
+    $hide_heading = $attributes['hideHeading'] ?? false;
 
-    /* --------------------------------------------------------------
-     * 3. Mark-up zurückgeben
-     * ------------------------------------------------------------ */
+    $heading_markup = '';
+    if (!$hide_heading) {
+      $heading_markup = sprintf(
+        '<h%1$d class="wp-block-fau-elemental-fau-meta-headline">%2$s</h%1$d>',
+        $headingLevel,
+        esc_html($heading)
+      );
+    }
+
     return sprintf(
       '<section aria-labelledby="%1$s" class="facts %2$s">
-<h%5$s class="wp-block-fau-elemental-fau-meta-headline">%3$s</h%5$s>
-            <ul class="facts__list" role="list">
-                %4$s
-            </ul>
-        </section>',
+        %3$s
+        <ul class="facts__list" role="list">
+            %4$s
+        </ul>
+    </section>',
       $heading_id,
       $wrapper_class,
-      esc_html($heading),
-      $innerBlocks,
-      $headingLevel
+      $heading_markup,
+      $innerBlocks
     );
   }
 }
