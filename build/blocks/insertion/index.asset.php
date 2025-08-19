acfca488f3718d8c12c66f09fdeef5e27be47554
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-08-19 09:41:17 +0200

    feat(insertion) - Finish the styling for insertion
    Finish

--- build/blocks/insertion/index.asset.php
+++ build/blocks/insertion/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '1f4105dadec804c4cd32');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => 'd2abefbc308583100932');
