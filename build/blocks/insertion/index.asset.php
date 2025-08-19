2998ce8a2a7110701aea8c8c70942043500457e7
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-08-19 13:15:16 +0200

    feat(insertion) - clearfix
    Allow usage within groups.

--- build/blocks/insertion/index.asset.php
+++ build/blocks/insertion/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '98e77d0298afe304b3dd');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '62d9f207b631fecd379a');
