7227932c8ce8137038d786baaf3c3d32b1d8255c
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-08-19 09:44:38 +0200

    feat(insertion) - Change the placeholder to core/image
    Change the Placeholder from text to image for better UX.

--- build/blocks/insertion/index.asset.php
+++ build/blocks/insertion/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => 'd2abefbc308583100932');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '98e77d0298afe304b3dd');
