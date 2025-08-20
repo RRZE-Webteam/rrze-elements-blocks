efcd754e18cc5f02bb476ad58da8350b6798ede4
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-08-20 16:10:21 +0200

    fix(accordion) - Improves the Accordion toggle behavior

--- build/blocks/insertion/index.asset.php
+++ build/blocks/insertion/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '62d9f207b631fecd379a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '5ece8fef8db8526f9bf3');
