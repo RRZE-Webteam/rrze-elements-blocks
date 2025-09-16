e28349c5b2f3f986bd9f428f4bc983ad51809ae0
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-09-16 11:15:40 +0200

    update(version) - Version 1.1.0
    Details s. Changelog

--- build/blocks/tab/index.asset.php
+++ build/blocks/tab/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-a11y', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ba500d934c1cd17cf118');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-a11y', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'a0037dcc1616de100888');
