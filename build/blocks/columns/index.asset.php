184b32568ef72cb2ed1c653c00d016e79342a28f
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-09-18 16:45:41 +0200

    fix(#304): Opts into the new default margins and paddings
    Opt in to the new default margins and paddings within the RangeControl and SliderControl components.

--- build/blocks/columns/index.asset.php
+++ build/blocks/columns/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f01f0290a46252ee50e2');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f22d0216029c5f4fe906');
