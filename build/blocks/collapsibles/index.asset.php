02049d99dea58172c48d8be76ff786bd7223bacc
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-09-18 16:38:35 +0200

    fix(#244): Fixes an issue within the CSS hierarchy
    Fixes #244, Fixes an issue where the selector got too specific to overwrite in other plugins.

--- build/blocks/collapsibles/index.asset.php
+++ build/blocks/collapsibles/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '331dc91390f8350fcc8d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '75324752a0eba8717669');
