ad322b7d917288936ed7daa73e919a621cff22bc
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-09-23 08:56:50 +0200

    fix(#326) - Fix Alert Styles in FAU-Elemental
    Fixes an issue where Alerts were styled wrong in combination with layout-flow and darkmode.

--- build/blocks/alert/index.asset.php
+++ build/blocks/alert/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => '4fd69ab976dcb50a982c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => 'df607dda7696447fbc68');
