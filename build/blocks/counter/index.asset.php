f09cb16fef4bb98aceb2302d8b9b83f9104f29d4
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-09-24 15:50:31 +0200

    fix(#333) - Fixes padding issue.
    Removes left padding from counter lists within the FAU Elemental theme to ensure correct layout. Fixes #333

--- build/blocks/counter/index.asset.php
+++ build/blocks/counter/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => '97dc7aa3f0f31a87700a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => 'ce174a51921691930d0c');
