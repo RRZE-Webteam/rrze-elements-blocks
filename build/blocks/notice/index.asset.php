51ed4fd2b4feb3e5aef6dd19efbc69cb48790244
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-09-24 15:56:49 +0200

    fix(#320) - Removes redundant notice styles
    Removes duplicate styles for notices to streamline the styling and improve maintainability. The removed styles are now handled in a more centralized way.

--- build/blocks/notice/index.asset.php
+++ build/blocks/notice/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-a11y', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c954fb38697745304d6c');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-a11y', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '522e1624f058bd0c1811');
