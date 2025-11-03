99714ed5f08a50e52d113ed44e0670b4baa6241c
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-11-03 13:34:54 +0100

    Fix(324 & 323) - Disables mouse interaction
    Disables mouse interaction on mirrored previews to prevent accidental interaction in the editor.
    
    This change ensures that the mirrored preview doesn't capture unintended clicks or hovers, providing a cleaner editing experience.

--- build/blocks/news/index.asset.php
+++ build/blocks/news/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '05274b5fea849caa143d');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '70c4625e037000089859');
