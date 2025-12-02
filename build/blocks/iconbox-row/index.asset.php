f5785843cd1411b5e0d8b702106763cff4b9a1ca
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-12-02 15:15:41 +0100

    fix(#356) - Resolve iconbox border issues inside the editor

--- build/blocks/iconbox-row/index.asset.php
+++ build/blocks/iconbox-row/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '939f11d88abb553ee882');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '8ace5524d8b557f969ed');
