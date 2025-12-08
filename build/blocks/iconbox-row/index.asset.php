451907abecb3f411c5e1cc110ef5c94d97468352
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-12-08 14:22:10 +0100

    fix(#356) - Resolve Iconbox and Counter Styles on Frontend

--- build/blocks/iconbox-row/index.asset.php
+++ build/blocks/iconbox-row/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '8ace5524d8b557f969ed');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '17c6efa80ef3e6431d7a');
