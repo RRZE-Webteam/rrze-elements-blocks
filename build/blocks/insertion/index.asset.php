923f84e95125adaf2650aa2447539241ca0fe925
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-12-08 14:42:44 +0100

    fix(#352): Corrects layout issues
    Addresses pull quote alignment and other layout
    inconsistencies in the rrze-elements blocks to
    improve the user experience and ensure visual
    harmony across different screen sizes and themes.
    
    Fixes #352

--- build/blocks/insertion/index.asset.php
+++ build/blocks/insertion/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => 'd2f21daccd420f78956f');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '0ff1e3aa7fa4768de2cb');
