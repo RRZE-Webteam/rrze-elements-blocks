5a7e12de4e2f500521bc09de04491b8b909b3633
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2026-01-14 15:07:08 +0100

    fix(#372) - Resolves an issue for Iconbox Blocks within groups
    Resolves an issue where WordPress Groups cause Iconbox containers to bleed into the unknonwn.

--- build/blocks/iconbox-row/index.asset.php
+++ build/blocks/iconbox-row/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'faef773cd13b4b424481');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c0671753f34baf59484a');
