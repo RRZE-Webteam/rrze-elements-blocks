ead4910207e798fb40f7b5b7776d33802a56fd26
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2026-01-29 19:46:18 +0100

    fix(#379) - Fixes style issues in FAU-Elemental
    Addresses style inconsistencies of the collapsibles block
    within the FAU-Elemental theme.
    
    This includes removing an unnecessary CSS file and adjusting
    the accordion styles for better compatibility and visual appeal.
    
    The changes ensure correct styling for both light and dark
    modes, addressing issues with borders, colors, and spacing.
    
    These adjustments enhance the user experience and maintain
    design consistency across the platform.

--- build/blocks/collapsibles/index.asset.php
+++ build/blocks/collapsibles/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'd53cbcd95b0a6895ebec');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '7e5026188bd509bac05a');
