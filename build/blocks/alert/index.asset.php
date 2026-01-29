6dc250e8dcc331b30cfe6bd54bb92acef07f7084
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2026-01-29 19:30:37 +0100

    fix(#378) - Resolves issues regarding alerts
    Forces Padding via !important temporarily.

--- build/blocks/alert/index.asset.php
+++ build/blocks/alert/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => '665600be95e449a9f214');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => '640bc504a281d42c1b14');
