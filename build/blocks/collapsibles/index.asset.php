3f8a3fdd385138160c0a75fb19e0b86aeb7b9093
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2026-03-03 17:23:51 +0100

    fix(css) - Fixes an issue with accordions in FAU-Elemental iFrame
    Resolves an issue with the background color of active tabs in FAU-Elemental. Part of #384

--- build/blocks/collapsibles/index.asset.php
+++ build/blocks/collapsibles/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '16ce98663f1ce5f8ee6f');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '83dfba6b160c63b8e831');
