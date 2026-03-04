0dec4f81a82f6bbc13c28384e6d17179909ba005
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2026-03-04 18:27:27 +0100

    fix(svg) - Fixes SVG syntax
    Part of #388

--- build/blocks/fact/index.asset.php
+++ build/blocks/fact/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-a11y', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => 'f4ee968c70be13a567bf');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-a11y', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => 'ba16c7aedfc3cad6dcfe');
