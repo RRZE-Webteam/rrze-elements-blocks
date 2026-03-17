60e66b95c30e84fca3bd5dedd789435b7fcf6098
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2026-03-17 09:13:05 +0100

    fix(InsertionEditorView) - Fixes the InsertionBlock inside the Editor
    Fixes an issue where users could not change the Image caption on left-aligned Insertions inside the BlockEditor. Closes #395

--- build/blocks/insertion/index.asset.php
+++ build/blocks/insertion/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '6ea4b5db0e6f08b62413');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '72340125781ed546ed8e');
