8160c56da7b962c75369ae6297356855eff14216
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2026-03-17 12:28:03 +0100

    fix(feedbackMechanism) - Improves UI for better User Feedback
    Fixes an issue where it was not obvious for users that an icon had to be selected. Part of #402

--- build/blocks/iconbox/index.asset.php
+++ build/blocks/iconbox/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-a11y', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => 'e09624612ea2ada753d9');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-a11y', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => '0df37952a379bf0326d8');
