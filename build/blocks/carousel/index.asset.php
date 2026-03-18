7ec8fe611a66f84932ef2f2af4b0986689ef2d01
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2026-03-18 15:17:29 +0100

    feat(carousel) - Define basic CSS markup
    This is part of #401

--- build/blocks/carousel/index.asset.php
+++ build/blocks/carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => 'bcc70de68193c9cc9e11');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => '6c2528af514716eee4dd');
--- src/blocks/carousel/edit.tsx
+++ src/blocks/carousel/edit.tsx
@@ -21,7 +21,7 @@ export default function Edit() {
             <ul aria-labelledby={"uniqueIDforTheSection-header"} role={"list"} className={"rrze-elements-blocks__carousel-content-list"}>
               <li className={"rrze-elements-blocks__carousel-content-list-item"} role={"listitem"} tabIndex={-1}>
                 <div className={"rrze-elements-blocks__carousel_feature-card-box"}>
-                  <div className={"rrze-elements-blocks__carousel_feature_card-content"}>
+                  <div className={"rrze-elements-blocks__carousel_feature_card-content"} style={{ position: 'relative', height: '680px', width: '320px' }}>
                     <h3 className={"rrze-elements-blocks__carousel_feature_card_subtitle"}>
                       Forschung & Lehre
                     </h3>
@@ -37,7 +37,85 @@ export default function Edit() {
                     </div>
                     <a className={"rrze-elements-blocks__carousel_feature_card_link"} href={""} aria-label={"Weitere Informationen zum Thema XYZ"} role={"button"}>
                       <div className={"rrze-elements-blocks__carousel_feature_card_link_control_container"}>
-                        <span className={"rrze-elements-blocks__carousel_feature_card_link_control_icon-container"}>
+                        <span className={"rrze-elements-blocks__carousel_feature_card_link_control_icon-container"} style={{ position: 'absolute' }}>
+                          <svg className={"rrze-elements-blocks__carousel_feature_card_link_control_icon"} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M440-120v-320H120v-80h320v-320h80v320h320v80H520v320h-80Z"/></svg>
+                        </span>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </li>
+              <li className={"rrze-elements-blocks__carousel-content-list-item"} role={"listitem"} tabIndex={-1}>
+                <div className={"rrze-elements-blocks__carousel_feature-card-box"}>
+                  <div className={"rrze-elements-blocks__carousel_feature_card-content"} style={{ position: 'relative', height: '680px', width: '320px' }}>
+                    <h3 className={"rrze-elements-blocks__carousel_feature_card_subtitle"}>
+                      Forschung & Lehre
+                    </h3>
+                    <p className={"rrze-elements-blocks__carousel_feature_card_text"}>Ein Studium für alle Fälle.</p>
+                    <div className={"rrze-elements-blocks__carousel_feature_card_bg"}>
+                      <figure className={"rrze-elements-blocks__carousel_feature_card_bg_figure"}>
+                        <picture className={"rrze-elements-blocks__carousel_feature_card_bg_figure_picture"}>
+                          <source srcSet={"https://plus.unsplash.com/premium_photo-1755883199872-2d31c8b8b012?q=80&w=2012&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} width={"1920"} height={"1080"} media={"(max-width: 734px)"} />
+                          <source srcSet={"https://plus.unsplash.com/premium_photo-1757682619735-ba211caa04f2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} width={"1920"} height={"1080"} />
+                          <img alt={"Alternativtext hier"} />
+                        </picture>
+                      </figure>
+                    </div>
+                    <a className={"rrze-elements-blocks__carousel_feature_card_link"} href={""} aria-label={"Weitere Informationen zum Thema XYZ"} role={"button"}>
+                      <div className={"rrze-elements-blocks__carousel_feature_card_link_control_container"}>
+                        <span className={"rrze-elements-blocks__carousel_feature_card_link_control_icon-container"} style={{ position: 'absolute' }}>
+                          <svg className={"rrze-elements-blocks__carousel_feature_card_link_control_icon"} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M440-120v-320H120v-80h320v-320h80v320h320v80H520v320h-80Z"/></svg>
+                        </span>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </li>
+              <li className={"rrze-elements-blocks__carousel-content-list-item"} role={"listitem"} tabIndex={-1}>
+                <div className={"rrze-elements-blocks__carousel_feature-card-box"}>
+                  <div className={"rrze-elements-blocks__carousel_feature_card-content"} style={{ position: 'relative', height: '680px', width: '320px' }}>
+                    <h3 className={"rrze-elements-blocks__carousel_feature_card_subtitle"}>
+                      Forschung & Lehre
+                    </h3>
+                    <p className={"rrze-elements-blocks__carousel_feature_card_text"}>Ein Studium für alle Fälle.</p>
+                    <div className={"rrze-elements-blocks__carousel_feature_card_bg"}>
+                      <figure className={"rrze-elements-blocks__carousel_feature_card_bg_figure"}>
+                        <picture className={"rrze-elements-blocks__carousel_feature_card_bg_figure_picture"}>
+                          <source srcSet={"https://plus.unsplash.com/premium_photo-1755883199872-2d31c8b8b012?q=80&w=2012&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} width={"1920"} height={"1080"} media={"(max-width: 734px)"} />
+                          <source srcSet={"https://plus.unsplash.com/premium_photo-1757682619735-ba211caa04f2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} width={"1920"} height={"1080"} />
+                          <img alt={"Alternativtext hier"} />
+                        </picture>
+                      </figure>
+                    </div>
+                    <a className={"rrze-elements-blocks__carousel_feature_card_link"} href={""} aria-label={"Weitere Informationen zum Thema XYZ"} role={"button"}>
+                      <div className={"rrze-elements-blocks__carousel_feature_card_link_control_container"}>
+                        <span className={"rrze-elements-blocks__carousel_feature_card_link_control_icon-container"} style={{ position: 'absolute' }}>
+                          <svg className={"rrze-elements-blocks__carousel_feature_card_link_control_icon"} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M440-120v-320H120v-80h320v-320h80v320h320v80H520v320h-80Z"/></svg>
+                        </span>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </li>
+              <li className={"rrze-elements-blocks__carousel-content-list-item"} role={"listitem"} tabIndex={-1}>
+                <div className={"rrze-elements-blocks__carousel_feature-card-box"}>
+                  <div className={"rrze-elements-blocks__carousel_feature_card-content"} style={{ position: 'relative', height: '680px', width: '320px' }}>
+                    <h3 className={"rrze-elements-blocks__carousel_feature_card_subtitle"}>
+                      Forschung & Lehre
+                    </h3>
+                    <p className={"rrze-elements-blocks__carousel_feature_card_text"}>Ein Studium für alle Fälle.</p>
+                    <div className={"rrze-elements-blocks__carousel_feature_card_bg"}>
+                      <figure className={"rrze-elements-blocks__carousel_feature_card_bg_figure"}>
+                        <picture className={"rrze-elements-blocks__carousel_feature_card_bg_figure_picture"}>
+                          <source srcSet={"https://plus.unsplash.com/premium_photo-1755883199872-2d31c8b8b012?q=80&w=2012&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} width={"1920"} height={"1080"} media={"(max-width: 734px)"} />
+                          <source srcSet={"https://plus.unsplash.com/premium_photo-1757682619735-ba211caa04f2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} width={"1920"} height={"1080"} />
+                          <img alt={"Alternativtext hier"} />
+                        </picture>
+                      </figure>
+                    </div>
+                    <a className={"rrze-elements-blocks__carousel_feature_card_link"} href={""} aria-label={"Weitere Informationen zum Thema XYZ"} role={"button"}>
+                      <div className={"rrze-elements-blocks__carousel_feature_card_link_control_container"}>
+                        <span className={"rrze-elements-blocks__carousel_feature_card_link_control_icon-container"} style={{ position: 'absolute' }}>
                           <svg className={"rrze-elements-blocks__carousel_feature_card_link_control_icon"} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M440-120v-320H120v-80h320v-320h80v320h320v80H520v320h-80Z"/></svg>
                         </span>
                       </div>
