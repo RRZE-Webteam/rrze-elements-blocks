2d7be4f4ff526b62c12b089a81473f45c9857170
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2026-03-31 14:36:38 +0200

    fix(jumpnameGeneration) - fix automatic jumpname generation
    Fixes #397

--- build/blocks/collapse/index.asset.php
+++ build/blocks/collapse/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-a11y', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '87854079a32c383d7877');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-a11y', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '3d338a72e731bb287bf2');
