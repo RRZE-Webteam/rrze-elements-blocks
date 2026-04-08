2b456e1bf9b28496a35e5003fd0a47bc0a41a20e
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2026-04-08 12:44:27 +0200

    feat(carousel) - Adds Support for Groups in FAU-Elemental
    This is part of #401 - Adds support for Dark Backgrounds in FAU-Elemental Theme.

--- build/blocks/carousel/index.asset.php
+++ build/blocks/carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e624c9820a2cef576df4');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '737484f292a4a9c0ccc8');
