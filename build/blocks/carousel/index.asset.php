bdde893e3061c0bbf499a11a00b94f45c16bcdcb
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2026-04-08 15:17:49 +0200

    feat(carousel) - Nesting Warning
    This is part of #401 for an Advanced Nesting Alert

--- build/blocks/carousel/index.asset.php
+++ build/blocks/carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '686791b8fff35e8a23b5');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '363a9dbacefba0f92f85');
