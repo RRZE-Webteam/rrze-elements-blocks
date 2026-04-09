a5f2e6d5509409398b319b77f95d22afcc3f5d33
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2026-04-09 09:19:15 +0200

    feat(info-card) - Fix Mobile and Tablet Previews
    This is part of #401

--- build/blocks/info-card/index.asset.php
+++ build/blocks/info-card/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '4cd6ed291644d5936b9b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '5c2f535435ed4cf527b9');
