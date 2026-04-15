87cfc2beaff72c5e1b6bf51d551ff8ed965c27ff
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2026-04-15 15:47:11 +0200

    fix(accordion) - Adjusts the Toggle Color for FAU-Elemental
    Adjusts the Toggle Color for Accordions. This closes #416

--- build/blocks/collapsibles/index.asset.php
+++ build/blocks/collapsibles/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'b8ca2bc7936521d26931');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ea59f1ea8801aacf5056');
