c9d971672d9f7652955528a214601687fe4434af
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2026-05-07 15:00:26 +0200

    feat(collapse) - Allow new Blocks for collapse
    This closes #424 - Allows rrze/appointment and rrze/research-data inside Accordions.

--- build/blocks/collapse/index.asset.php
+++ build/blocks/collapse/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-a11y', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '3d338a72e731bb287bf2');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-a11y', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '6c6b58078f7c1d81f44f');
