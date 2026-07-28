7147231f34dceea2f059b5ec795d2327f3262af4
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2026-07-28 13:51:11 +0200

    feat(process) - Add styling inverted
    Adds Styling for is-style-dark groups. This is part of #432

--- build/blocks/process-step/index.asset.php
+++ build/blocks/process-step/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '50fd21befbee7c3261b4');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9e99b363b1748456b784');
