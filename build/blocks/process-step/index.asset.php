a1ecfe223ce2b51c28bb6172f6b31a39c3448eba
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2026-07-28 16:17:22 +0200

    feat(process) - Add styling
    Part of #432 - Adds Mixin for the SVG Background

--- build/blocks/process-step/index.asset.php
+++ build/blocks/process-step/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9e99b363b1748456b784');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b19e888ae1437f6ce8da');
