3ff3b2c39c8b6cdd46107b4dd4126177e9dc1695
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2026-09-04 12:42:57 +0200

    fix(process) - Fixes Images inside the process block

--- build/blocks/process-step/index.asset.php
+++ build/blocks/process-step/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '21c300d4108790a086db');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '29586e3f71f4c7ce5575');
